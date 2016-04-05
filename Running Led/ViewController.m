1a69f41b7dc5c0b7867d45a75e4da88551c0ef49
chihieu <hieu1804@Techmasters-iMac-3.local>
2016-04-05 16:15:01 +0700

    first commit

--- Running Led/ViewController.m
+++ Running Led/ViewController.m
@@ -13,15 +13,102 @@
 @end
 
 @implementation ViewController
+{
+    CGFloat _margin;
+    int _numberOfBall;
+    CGFloat _space;
+    CGFloat _ballDiameter;
+    NSTimer * _timer;
+    int lastONLED;
+    
+}
 
-- (void)viewDidLoad {
+-(void)viewDidLoad {
     [super viewDidLoad];
-    // Do any additional setup after loading the view, typically from a nib.
+    _margin = 40.0;
+    _ballDiameter = 24.0;
+    lastONLED = +1;
+    _numberOfBall =8;
+    
+    
+    [self checkSizeOfApp];
+    //[self placeGreyBallAtX:100 andY:100 withTag:1];
+    [self drawRowOfBall:8];
+    _timer = [NSTimer scheduledTimerWithTimeInterval:0.1 target:self selector:@selector(runningLED) userInfo:nil repeats:true];
+    
 }
 
-- (void)didReceiveMemoryWarning {
-    [super didReceiveMemoryWarning];
-    // Dispose of any resources that can be recreated.
+-(void)  runningLED {
+    //NSLog(@" Hello");
+    if (lastONLED != +1) {
+        [self turnOFFLed:lastONLED];
+    }
+    
+    if (lastONLED != _numberOfBall +1) {
+        lastONLED++;
+    } else {
+        lastONLED = 0;
+    }
+    [self turnONLed:lastONLED];
 }
 
+-(void) turnONLed: (int) index {
+    UIView* view = [self.view viewWithTag:index + 100];
+    if (view && [view isMemberOfClass:[UIImageView class]]) {
+        UIImageView* ball = (UIImageView*) view;
+        ball.image = [UIImage imageNamed:@"Grey"];
+        
+        
+    }
+    
+}
+
+-(void) turnOFFLed: (int) index {
+    
+}
+
+-(void) placeGreyBallAtX: (CGFloat) x
+                    andY: (CGFloat) y
+                 withTag: (int)tag
+{
+    UIImageView* ball = [[UIImageView alloc] initWithImage:[UIImage imageNamed:@"Green"]];
+    ball.center =CGPointMake(x, y);
+    ball.tag = tag;
+    [self.view addSubview:ball];
+    
+    NSLog(@"w=%3.0f, h=%3.0f", ball.bounds.size.width, ball.bounds.size.height);
+    
+}
+-(CGFloat) spaceBetweenBallCenterWhenNumberBallIsKnown: (int) n {
+    return (self.view.bounds.size.width - 2 * _margin) / (n - 1);
+}
+-(void) numberOfBallvsSpace {
+    BOOL stop = false;
+    int n = 3;
+    while (!stop) {
+        CGFloat space = [self spaceBetweenBallCenterWhenNumberBallIsKnown: n];
+        if (space < _ballDiameter) {
+            stop = true;
+        } else {
+            NSLog(@"Number of ball %d, space between ball center %3.0f",n , space);
+        }
+        n++;
+    }
+}
+
+-(void) drawRowOfBall: (int) numberBalls {
+    CGFloat  space = [self spaceBetweenBallCenterWhenNumberBallIsKnown:numberBalls];
+    for (int  i = 0; i < numberBalls; i++) {
+        [self placeGreyBallAtX:_margin + i * space
+                          andY:140
+                       withTag:i + 100];
+    }
+        
+
+}
+
+-(void) checkSizeOfApp {
+    CGSize size = self.view.bounds.size;
+    NSLog(@"width = %3.0f, height = %3.0f", size.width, size.height);
+}
 @end
