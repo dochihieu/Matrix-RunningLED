7861445a21ad743d61162f0c7ea0dacd67791833
chihieu <hieu1804@techmasters-imac-3.local>
2016-04-05 14:27:31 +0700

    Initial Commit

---
+++ Running Led/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Running Led
+//
+//  Created by chihieu on 4/5/16.
+//  Copyright © 2016 chihieu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Running Led/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Running Led
+//
+//  Created by chihieu on 4/5/16.
+//  Copyright © 2016 chihieu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
